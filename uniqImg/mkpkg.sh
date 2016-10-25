b9d664962d8733cb43e167f678c46c42f5f35bfd
chenyvehtung <chenyvehtung@163.com>
2016-10-25 15:40:22 +0800

    add build script for uniqimg

---
+++ uniqImg/mkpkg.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+APP_PATH='usr/share/applications'
+ICON_PATH='usr/share/pixmaps'
+SRC_PATH='usr/share/uniqimg'
+
+rm -rf ./pkg/
+rm -rf ./*.deb
+mkdir -p ./pkg/$APP_PATH
+mkdir -p ./pkg/$ICON_PATH
+mkdir -p ./pkg/$SRC_PATH
+cp -rf ./DEBIAN ./pkg/
+
+cp -f ./src/*.py ./pkg/$SRC_PATH/
+mkdir ./pkg/$SRC_PATH/backend
+cp -f ./src/backend/*.py ./pkg/$SRC_PATH/backend/
+mkdir ./pkg/$SRC_PATH/ui
+cp -f ./src/ui/*.py ./pkg/$SRC_PATH/ui/
+
+cp -f ./src/image/icon.png ./pkg/$ICON_PATH/uniqimg.png
+cp -f ./src/uniqimg.desktop ./pkg/$APP_PATH/
+
+dpkg -b ./pkg uniqimg_0.1.0_all.deb
